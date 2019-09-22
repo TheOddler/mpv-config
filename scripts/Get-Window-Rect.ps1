bb244399a8a792d96229086f5b253b0226e23eda
Pablo Bollansée <pablo.bollansee@gmail.com>
2019-09-22 12:22:54 +0200

    Updated to much smoother opening - Geometry is now used at startup since this is instant so you won't see an initial window in the middle of your screen before it jumps to the right place - Rewrote the powershell script to get the position of the client rect (this doesn't include the decorations around the window) since that's what Geometry needs

--- scripts/Get-Window-Rect.ps1
+++ scripts/Get-Window-Rect.ps1
@@ -5,6 +5,12 @@ public class Window {
     [DllImport("user32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool ClientToScreen(IntPtr hWnd, ref POINT lpPoint);
 }
 public struct RECT
 {
@@ -13,9 +19,33 @@ public struct RECT
     public int Right;
     public int Bottom;
 }
+public struct POINT
+{
+    public int x;
+    public int y;
+}
 "@
 
 $Handle = (Get-Process -Id $Args[0]).MainWindowHandle
 $WindowRect = New-Object RECT
-$GotWindowRect = [Window]::GetWindowRect($Handle, [ref]$WindowRect)
+$ClientRect = New-Object RECT
+$TopLeft = New-Object POINT
+$BottomRight = New-Object POINT
+
+# $GotWindowRect = [Window]::GetWindowRect($Handle, [ref]$WindowRect)
+
+[Window]::GetClientRect($Handle, [ref]$ClientRect) | out-null
+$TopLeft.x = 0
+$TopLeft.y = 0
+$BottomRight.x = $ClientRect.Right
+$BottomRight.y = $ClientRect.Bottom
+
+[Window]::ClientToScreen($Handle, [ref]$TopLeft) | out-null
+[Window]::ClientToScreen($Handle, [ref]$BottomRight) | out-null
+
+$WindowRect.Left = $TopLeft.x
+$WindowRect.Top = $TopLeft.y
+$WindowRect.Right = $BottomRight.x
+$WindowRect.Bottom = $BottomRight.y
+
 ConvertTo-Json($WindowRect)
--- scripts/remember_last_window_rect.js
+++ scripts/remember_last_window_rect.js
@@ -1,47 +1,24 @@
 // Some setup used by both reading and writing
 var dir = mp.utils.split_path(mp.get_script_file())[0]
-var file_path = mp.utils.join_path(dir, "last_window_rect.txt")
-var pid = mp.utils.getpid()
-
-function run_script(name, args)
-{
-    var ps1_script = mp.utils.join_path(dir, name)
-    var args_base = ["powershell", "&(\"" + ps1_script + "\")"]
-    var result = mp.utils.subprocess({args: args_base.concat(args), cancellable: false})
-    return result.stdout
-}
+var rect_path = mp.utils.join_path(dir, "last_window_rect.txt")
 
 // Read last window rect if present
-function load_rect()
-{
-    try
-    {
-        var rect_json = mp.utils.read_file(file_path)
-        var rect = JSON.parse(rect_json)
-        run_script("Set-Window-Rect.ps1", [
-                pid,
-                rect.Left, // x
-                rect.Top, // y
-                rect.Right - rect.Left, // width
-                rect.Bottom - rect.Top // height
-            ])
+try {
+    var rect_json = mp.utils.read_file(rect_path)
+    var rect = JSON.parse(rect_json)
 
-        // var width = rect.Right - rect.Left
-        // var height = rect.Bottom - rect.Top
-        // mp.set_property("screen", 0)
-        // mp.set_property("geometry", width + "x" + height + "+" + rect.Left + "+" + rect.Top)
-    }
-    catch (e)
-    {
-        dump(e)
-    }
+    var width = rect.Right - rect.Left
+    var height = rect.Bottom - rect.Top
+    mp.set_property("screen", 0)
+    mp.set_property("geometry", width + "x" + height + "+" + rect.Left + "+" + rect.Top)
 }
-mp.register_event("start-file", load_rect)
+catch (e) {
+    dump(e)
+}
 
-// Write the window rect on shutdown
 function save_rect() {
-    var rect_json = run_script("Get-Window-Rect.ps1", [pid])
-    mp.utils.write_file("file://" + file_path, rect_json)
+    var ps1_script = mp.utils.join_path(dir, "Get-Window-Rect.ps1")
+    var rect_json = mp.utils.subprocess({ args: ["powershell", "&(\"" + ps1_script + "\")", mp.utils.getpid()], cancellable: false }).stdout
+    mp.utils.write_file("file://" + rect_path, rect_json)
 }
 mp.register_event("shutdown", save_rect)
-mp.register_event("end-file", save_rect)
